04e3e94681e91ba4707a8e32e61e58e20d1aeb1e
Claire Annan <claire.annan.me@gmail.com>
2015-10-07 06:25:54 -0400

    Moving all functions outside of doc.ready

--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
+var counter = 0;
+
+function newGame() {
+	//reset counter, status, steps and details
+	console.log('Start a new game!');
+	counter = 0;
+}
+
+function progress() {
+	counter++;
+	// do fancy class changes on nav#progress .bar + .stops
+}
+
+var step = {
+	id: 1,
+	title: function() {
+		return 'Question ' + this.id;
+	},
+	question: 'Dolore, a excepturi! Officia distinctio, harum. Cum aut quo optio ips?',
+	options: ['Option one', 'Option two', 'Option three', 'Option four'],
+	answer: 1,
+	checkAnswer: function(checked) {
+		checked = parseInt(checked);
+		if(checked !== this.answer) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	},
+	detail: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi rerum aliquam qui commodi, alias quidem minus ex odit, nulla, magnam recusandae et quibusdam laborum error ipsam. Delectus dolores sequi molestiae.',
+};
+
+var step1 = Object.create(step); // why are we creating objects when the questions share no common values?
+
+
 $(document).ready(function(){
 // Do all my functions need to be in the ready function?
-
-	var counter = 0;
-
-	function newGame() {
-		//reset counter, status, steps and details
-		console.log('Start a new game!');
-		counter = 0;
-	}
-
-	function progress() {
-		counter++;
-		// do fancy class changes on nav#progress .bar + .stops
-	}
-
-	var step = {
-		id: 1,
-		title: function() {
-			return 'Question ' + this.id;
-		},
-		question: 'Dolore, a excepturi! Officia distinctio, harum. Cum aut quo optio ips?',
-		options: ['Option one', 'Option two', 'Option three', 'Option four'],
-		answer: 1,
-		checkAnswer: function(checked) {
-			checked = parseInt(checked);
-			if(checked !== this.answer) {
-				return false;
-			}
-			else {
-				return true;
-			}
-		},
-		detail: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi rerum aliquam qui commodi, alias quidem minus ex odit, nulla, magnam recusandae et quibusdam laborum error ipsam. Delectus dolores sequi molestiae.',
-	};
-
-	var step1 = Object.create(step); // why are we creating objects when the questions share no common values?
-
 	$('#start').click(newGame);
 
 	$('.step-title').text(step1.title());
