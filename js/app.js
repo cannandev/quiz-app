d765381821ee1021c9d1ea64be979edef6b76adc
Claire Annan <claire.annan.me@gmail.com>
2015-10-07 05:39:28 -0400

    Adding checkAnswer function. Display results.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
 // Do all my functions need to be in the ready function?
 
+	var counter = 0;
+
 	function newGame() {
 		//reset counter, status, steps and details
 		console.log('Start a new game!');
+		counter = 0;
+	}
+
+	function progress() {
+		counter++;
+		// do fancy class changes on nav#progress .bar + .stops
 	}
 
 	var step = {
@@ -13,18 +21,36 @@ $(document).ready(function(){
 		},
 		question: 'Dolore, a excepturi! Officia distinctio, harum. Cum aut quo optio ips?',
 		options: ['Option one', 'Option two', 'Option three', 'Option four'],
-		answer: function() {
-			return this.options[1];
+		answer: 1,
+		checkAnswer: function(checked) {
+			checked = parseInt(checked);
+			if(checked !== this.answer) {
+				return false;
+			}
+			else {
+				return true;
+			}
 		},
 		detail: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi rerum aliquam qui commodi, alias quidem minus ex odit, nulla, magnam recusandae et quibusdam laborum error ipsam. Delectus dolores sequi molestiae.',
 	};
 
 	var step1 = Object.create(step); // why are we creating objects when the questions share no common values?
 
-	$('.start').click(newGame);
+	$('#start').click(newGame);
 
 	$('.step-title').text(step1.title());
 	$('.step-question').text(step1.question);
-	$('.step-detail p').text(step1.detail);
-
+	$('#submit').click(function(e){
+		e.preventDefault();
+		var checked = $('input:checked').val();
+		if (step1.checkAnswer(checked)) {
+			$('.step-detail h1').text('Hey, Yurright!');
+			progress();
+		}
+		else {
+			$('.step-detail h1').text('Get outta here.');
+		}
+		$('.step-detail p').text(step1.detail);
+	});
+	
 });
