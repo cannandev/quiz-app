a6cd7327900c470b34d62a55967aeed8f4e2d818
Claire Annan <claire.annan.me@gmail.com>
2015-10-07 04:21:08 -0400

    Layout structure for quiz-app.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
+// Do all my functions need to be in the ready function?
 	console.log('Ready!');
+
+	function newGame() {
+		//reset counter, status, steps and details
+	}
 });
