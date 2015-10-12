1c462ab0e8da19440ce62015a7e83b61ead3d505
Claire Annan <claire.annan.me@gmail.com>
2015-10-11 20:21:14 -0400

    Add questions array of objects.

--- js/mvc-app.js
+++ js/mvc-app.js
@@ -2,6 +2,43 @@
  * It turns the function declaration into function expression. */
 
 (function(){
+	var questions = [
+		{
+			title: 'Question 1',
+			question: 'What was the city was originally called?',
+			choices: ['New Germany', 'New Amsterdam', 'New London', 'New Jack City'],
+			answer: 1,
+			detail: 'New Amsterdam was a 17th-century Dutch settlement established at the southern tip of Manhattan Island. It was renamed New York on September 8, 1664, in honor of the Duke of York in whose name the English had captured it.',	
+		},
+		{
+			title: 'Question 2',
+			question: 'Which of these is NOT a borough?',
+			choices: ['Brooklyn', 'Staten Island', 'Bronx', 'Yonkers'],
+			answer: 3,
+			detail: 'New York City is divided into five different boroughs, or neighborhoods. They are Manhattan, the Bronx, Queens, Brooklyn and Staten Island.',	
+		},
+		{
+			title: 'Question 3',
+			question: 'Where is Brooklyn\'\s original Chinatown?',
+			choices: ['Williamsburg', 'Flatbush', 'Sunset Park', 'Sheepshead Bay'],
+			answer: 2,
+			detail: 'The first Brooklyn Chinatown was originally established in the Sunset Park area of the New York City borough of Brooklyn. The borough has since evolved three larger Chinatowns, between Sunset Park, Bensonhurst, and Avenue U in Sheepshead Bay.',
+		},
+		{
+			title: 'Question 4',
+			question: 'Broadway originally shared a name with which famous store?',
+			choices: ['Bloomingdale\'\s', 'Macy\'\s', 'Kmart', 'Walmart'],
+			answer: 0,
+			detail: 'In the 18th century, Broadway ended at the town commons north of Wall Street, where traffic continued up the West Side via Bloomingdale Road. On February 14, 1899, the name "Broadway" was extended to the entire Broadway/Bloomingdale/Boulevard road.',	
+		},
+		{
+			title: 'Question 5',
+			question: 'What is another popular name for Times Square?',
+			choices: ['The Crossroads of the City','The Center of the Planet','The Great White Way', 'The Giant Lightbulb'],
+			answer: 2,
+			detail: 'This nickname came about because Broadway was one of the first streets in the United States to be lit with electric lights.',	
+		},		
+	];
 
 	var gameView = {
 		startButton: '',
@@ -27,15 +64,18 @@
 			this.startButton.click(gameController.newGame);
 			this.submitButton.click(function(e){
 				e.preventDefault();
-				gameController.getUserChoice();				
+				gameController.submitUserChoice();				
 			});
 		},
 		render: function(){
-			this.title.text(gameData.title);
-			this.question.text(gameData.question);
+console.log('counter: ' + gameData.counter);			
+
+			var q = gameData.questions[gameData.counter]; // Is this a good way?
+			this.title.text(q.title);
+			this.question.text(q.question);
 			this.choicesList.empty();
-			for(var i=0; i < gameData.choices.length; i++) {
-				$(this.choicesList).append('<label><input type="radio" name="choicesRadios" value="' + i + '">' + gameData.choices[i] + '</label>');
+			for(var i=0; i < q.choices.length; i++) {
+				$(this.choicesList).append('<label><input type="radio" name="choicesRadios" value="' + i + '">' + q.choices[i] + '</label>');
 			}			
 		},
 		showDetail: function(d){
@@ -50,25 +90,22 @@
 
 	var gameData = {
 		counter: '',
-		title: 'Question 1',
-		question: 'What was the city was originally called?',
-		choices: ['New Germany', 'New Amsterdam', 'New London', 'New Jack City'],
-		answer: 1,
-		detail: 'New Amsterdam was a 17th-century Dutch settlement established at the southern tip of Manhattan Island. It was renamed New York on September 8, 1664, in honor of the Duke of York in whose name the English had captured it.',	
 		totalCorrect: '',
 		userChecked: '',
+		questions: questions,
 		resetVariables: function(){
-			console.log('resetVariables');
 			this.counter = 0;
 			this.totalCorrect = 0;
-			//all kinds of goodies
+			gameView.progressStops.addClass('fa-circle-o').removeClass('fa-circle');
+			gameView.progressBar.animate({'width':'0%'}, 'slow');
+			gameView.form.fadeIn('slow');
 			gameView.render();
+			$('.step-detail').hide();
 		},
 		checkAnswer: function(){
 			this.userChecked = $('input:checked').val();
 			this.userChecked = parseInt(this.userChecked);
-			console.log(this.userChecked);
-			if (this.userChecked !== this.answer) {
+			if (this.userChecked !== this.questions[this.counter].answer) {
 				gameView.detail.text('Get outta here.');
 			}
 			else {
@@ -76,7 +113,8 @@
 				gameView.detail.text('Hey, Yurright!');
 				gameView.drawProgress(this.totalCorrect);								
 			}
-			gameView.showDetail(this.detail); // Not sure if this is View or Data			
+			this.counter++;
+			gameView.showDetail(this.questions[this.counter].detail); // Not sure if this is View or Data			
 		},
 	};
 
@@ -85,16 +123,12 @@
 			gameView.init();
 			gameController.newGame();
 		},
-		getUserChoice: function(){
+		submitUserChoice: function(){
 			gameData.checkAnswer();
+			gameView.render();
 		},
 		newGame: function(){
-			console.log('in newGame');			
-			gameView.render();
-			gameView.progressStops.addClass('fa-circle-o').removeClass('fa-circle');
-			gameView.progressBar.animate({'width':'0%'}, 'slow');
-			gameView.form.fadeIn('slow');
-			$('.step-detail').hide();	
+			gameData.resetVariables();	
 		},
 	};
 
